daca51944fbd77b0dd3261c5aebcc741cd4c20bc
Adi Yehezkeli <adi.yehezkeli@gmail.com>
2018-04-21 20:11:40 +0300

    ex1

---
+++ app/src/main/java/com/adiyehezkeli/selfchatadi/MsgAdapter.java
@@ -0,0 +1,65 @@
+package com.adiyehezkeli.selfchatadi;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.adiyehezkeli.selfchatadi.Message;
+import com.adiyehezkeli.selfchatadi.R;
+
+import java.util.ArrayList;
+
+public class MsgAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private LayoutInflater mLayoutInflater;
+    private ArrayList<Message> msgArray;
+
+    public MsgAdapter(Context context, ArrayList<Message> msgs)
+    {
+        mContext = context;
+        msgArray = msgs;
+        mLayoutInflater = (LayoutInflater) mContext.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return msgArray.size();
+    }
+
+    @Override
+    public Message getItem(int i) {
+        return msgArray.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View rowItem = mLayoutInflater.inflate(R.layout.li_msg, parent, false);
+
+        TextView msgName = (TextView) rowItem.findViewById(R.id.list_name);
+        TextView msgText = (TextView) rowItem.findViewById(R.id.list_text);
+        TextView msgTime = (TextView) rowItem.findViewById(R.id.list_time);
+
+        Message msgObj = getItem(position);
+
+        msgName.setText(msgObj.getName());
+        msgText.setText(msgObj.getMessage());
+        msgTime.setText(msgObj.getTimeStamp());
+
+        return rowItem;
+    }
+
+    public void addMsg(Message newMsg)
+    {
+        msgArray.add(newMsg);
+        notifyDataSetChanged();
+    }
+}
